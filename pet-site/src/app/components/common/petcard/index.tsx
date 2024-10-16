1455abb6897d49a2690926bdd01b9f1646ed1106
Romesh Perera <91802374+romesh97@users.noreply.github.com>
2024-10-16 23:11:09 +0000

    layout changes / added pet card

---
+++ pet-site/src/app/components/common/petcard/index.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+export default function PetCard() {
+    <Box sx={{ px: "20px", pb: 4, pt: 2.5 }}>
+        <Typography>
+            Sample Card
+        </Typography>
+    </Box>
+}
