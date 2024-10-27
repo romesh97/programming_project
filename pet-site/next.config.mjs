1b40c5ac28bd324a6ae84fe57c6dfd2641d2093c
Romesh Perera <91802374+romesh97@users.noreply.github.com>
2024-10-27 01:02:11 +0000

    next config changes

--- pet-site/next.config.mjs
+++ pet-site/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    typescript: {
+      ignoreBuildErrors: true,
+    },};
 
 export default nextConfig;
