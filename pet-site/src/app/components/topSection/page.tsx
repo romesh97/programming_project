717d6f8a49a8069f15a25be7ec187ed5e31e3519
Romesh Perera <91802374+romesh97@users.noreply.github.com>
2024-10-28 23:48:57 +0000

    about page changes

--- pet-site/src/app/components/topSection/page.tsx
+++ pet-site/src/app/components/topSection/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import Image from "next/image";
 
 //TODO: Fix icons issue
@@ -15,21 +15,40 @@ const TopSection: React.FC = () => {
       >
         <Image
           src="/images/Group 2.svg"
-         // layout="fit"
+          // layout="fit"
           alt="dogs breed"
           width={450}
           height={200}
-          style={{objectFit: "contain"}}
+          style={{ objectFit: "contain" }}
         />
         <Image
           src="/images/hero_dog.svg"
-         // layout="fit"
+          // layout="fit"
           alt="Hero Image"
           width={600}
           height={300}
-          style={{objectFit: "contain"}}
+          style={{ objectFit: "contain" }}
         />
       </Stack>
+      {/* Description Section */}
+      <Box
+        sx={{
+          background: "#3D3D3D",
+          width: "100%",
+          color: "rgba(255, 255, 255, 0.60);",
+          px: { sm: "200px", xs: "80px" },
+          py: { sm: "150px", xs: "40px" },
+        }}
+      >
+        <Typography variant="body1" align="center" letterSpacing="0.6px">
+          Join a lively pet lovers' community to share your passion for all
+          kinds of pets. Show off your pet's cute moments, connect with other
+          pet enthusiasts, or find care tips and inspiration. Pet-Site is your
+          destination to celebrate the joy pets bring. Be part of a community
+          that cherishes the bond between pets and their owners. Connect, share,
+          and enjoy the journey together!
+        </Typography>
+      </Box>
     </Box>
   );
 };
