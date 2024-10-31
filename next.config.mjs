f4d260dce45789212c1dd682b638f40c822ed404
Romesh Perera <91802374+romesh97@users.noreply.github.com>
2024-10-31 18:34:49 +0000

    adjust config file to accept firebase storage

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,17 @@ const nextConfig = {
     swcMinify: true,
     typescript: {
       ignoreBuildErrors: true,
-    },};
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "firebasestorage.googleapis.com",
+          port: "",
+          pathname: "/v0/b/**",
+        },
+      ],
+    },
+  };
 
 export default nextConfig;
